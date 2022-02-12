9a7fa3a2b399b3c3e5e62eec3c7a437ee37ad392
arif1205 <imdariful71@gmail.com>
2022-02-12 23:13:17 +0600

    resolve conflicts

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import DashboardItem from '../DashboardItem/DashboardItem';
 import { DashboardWrapper } from './Dashboard.styles';
 
 const Dashboard = () => {
   return (
     <DashboardWrapper>
-      
+      <DashboardItem />
     </DashboardWrapper>
   );
 };
---
+++ src/data/dashboardItemdata.js
@@ -0,0 +1,30 @@
+export const dashboardItemsData = [
+  {
+    title: " wMEMO Price",
+    price: "$25139.58",
+  },
+  {
+    title: " Market Cap",
+    price: "$567,662,877",
+  },
+  {
+    title: " TVL",
+    price: "$372,680,206",
+  },
+  {
+    title: " APY",
+    price: "80,698.7%",
+  },
+  {
+    title: " Current Index",
+    price: "17.12 TIME",
+  },
+  {
+    title: " Treasury Balance",
+    price: "$717,796,421",
+  },
+  {
+    title: " Backing per $wMEMO",
+    price: "$33,965",
+  },
+];
